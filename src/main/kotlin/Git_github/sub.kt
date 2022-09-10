9b8a1f20331d134a4c428430701541563cb822bc
kaushik karan singh <kaushikkaransinghkks@gmail.com>
2022-09-10 11:47:56 +0530

    Sum file

--- src/main/kotlin/Git_github/sub.kt
+++
@@ -1,9 +0,0 @@
-package Git_github
-
- fun main(){
-
-     val a =45
-     val b= 56
-
-     println(a-b)
- }
---
+++ src/main/kotlin/Kotlin_practice/Kotlin_basic/sum.kt
@@ -0,0 +1,10 @@
+package Kotlin_practice.Kotlin_basic
+
+
+ fun main (){
+     var a =  34
+     var b = 45
+
+     println(a + b)
+
+ }
